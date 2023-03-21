db3306008d581eb547881c004db63d3d89e35ba6
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-21 20:26:42 -0300

    Update App.js

--- 01 React basico/project-1/src/App.js
+++ 01 React basico/project-1/src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá Mundo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component} from 'react';
+// stateless não tem estado
+// componente de class // o componente de classe tem que ter um metodo render e retornar JSX
+class App extends Component {
+    render(){
+      return (
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Olá Mundo
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </header>
+            </div>
+          );
+    };
 }
+
+// stateless // componente de função retornando JSX
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Olá Mundo
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
