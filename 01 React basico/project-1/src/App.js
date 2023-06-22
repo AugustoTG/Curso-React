636358af713f82b45a4ee9d736bb9f307423c67e
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-06-22 19:35:38 -0300

    Update App.js

--- 01 React basico/project-1/src/App.js
+++ 01 React basico/project-1/src/App.js
@@ -1,40 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Component} from 'react';
 // stateless não tem estado
 // componente de class // o componente de classe tem que ter um metodo render e retornar JSX
 class App extends Component {
-    constructor(props){
-      super(props);
-      this.handlePClick = this.handlePClick.blind(this);
-      this.state = {
-        name: 'Augusto Tavares'
-      };
-    }
-    handlePClick(){
-      this.setState({name:'Júnior'});
-    }
-    render(){
-      const {name} = this.state;
-      return (
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p onClick={this.handlePClick}>
-                  Olá Mundo, me chamo {name}
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-              </header>
-            </div>
-          );
-    };
+  state = {
+    counter: 0,
+    posts: [
+      {id: 1,
+        title: 'O Título 1',
+        body: 'O corpo 1'
+      },
+      {id: 2,
+        title: 'O Título 2',
+        body: 'O corpo 2'
+      },
+      {id: 3,
+        title: 'O Título 3',
+        body: 'O corpo 3'
+      }
+    ]
+  };
+
+  handleTimeOut = ()=>{
+    const {posts, counter} = this.state;
+    posts[0].title = 'O título mudou!';
+    setTimeout(()=>{
+      this.setState({posts, counter: counter + 1});
+    }, 1000);
+  }
+
+  componentDidMount(){
+    this.handleTimeOut();
+  };
+
+  componentDidUpdate(){
+    this.handleTimeOut();
+  }
+
+  render(){
+    const {posts, counter} = this.state;
+    return (
+      <div className="App">
+        <h1>{counter}</h1>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
 }
 
 // stateless // componente de função retornando JSX
