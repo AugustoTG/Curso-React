0de062326a6f1c8c7bdb9b2975957a2910ce1ed4
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-07-10 18:08:34 -0300

    update files

--- 01 React basico/project-1/src/components/PostCard/index.jsx
+++ 01 React basico/project-1/src/components/PostCard/index.jsx
@@ -1,9 +1,11 @@
-export const PostCard = ({ title, cover, body}) => (
-    <div className="post">
-      <img src={cover} alt={title} />
-      <div className="post-content">
-        <h1>{title}</h1>
-        <p>{body}</p>
-      </div>
+import './styles.css';
+
+export const PostCard = ({ title, cover, body, id }) => (
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h2>{title} {id}</h2>
+      <p>{body}</p>
     </div>
-  );
+  </div>
+);
